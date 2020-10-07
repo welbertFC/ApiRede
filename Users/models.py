c11c9603633c219d0d5bbcd17c562ae65d3d9ead
Welbert Fernandes Costa <welbert.jsj@gmail.com>
2020-10-07 00:23:42 -0300

    Insert User

--- Users/models.py
+++ Users/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    email = models.EmailField()
+    senha = models.CharField(max_length=32)
+    criacao = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        abstract = True
+
+class Usuario(User):
+    nome = models.CharField(max_length=150)
+    data_nasc = models.DateField()
+    igreja = models.CharField(max_length=200, null=True, blank=True)
+    congregacao = models.CharField(max_length=200, null=True, blank=True)
+    endereco = models.TextField(null=True, blank=True)
+    cidade = models.CharField(max_length=70)
+    estado = models.CharField(max_length=70)
+    administrador = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.nome
